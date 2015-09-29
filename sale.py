d1ee3f7d2eb759dc4d7c0570ca904d57ff9bb73d
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2015-09-29 16:48:08 +0530

    Implemented apply shipping method for carrier type product

--- sale.py
+++ sale.py
@@ -5,7 +5,8 @@
     :copyright: (c) 2015 by Fulfil.IO Inc.
     :license: see LICENSE for details.
 """
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
 
 __metaclass__ = PoolMeta
 
@@ -17,7 +18,23 @@ class Sale:
         """
         Apply shipping for sale
         """
-        if self.carrier:
+        Currency = Pool().get('currency.currency')
+
+        if not self.carrier:
+            return
+
+        if self.carrier.carrier_cost_method == 'product':
+            with Transaction().set_context(self._get_carrier_context()):
+                shipment_cost, currency_id = self.carrier.get_sale_price()
+
+            shipment_cost = Currency.compute(
+                Currency(currency_id), shipment_cost, self.currency
+            )
+            self.add_shipping_line(
+                shipment_cost,
+                self.carrier.rec_name
+            )
+        else:
             getattr(
                 self, 'apply_%s_shipping' % self.carrier.carrier_cost_method
             )()
